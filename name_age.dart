91e7463fa92974e5a5b34648fde6689660e0316f
vaishnavi222222 <151459102+vaishnavi222222@users.noreply.github.com>
2025-03-11 23:15:09 +0530

    Create name_age.dart

---
+++ name_age.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+void main(){
+  stdout.write ("Enter your name:");
+  string name = stdin.readLimeSync()!);
+  stdout.cwrite ("Enter your age: ");
+  string age = stdin.readLineSync()!);
+  print(name);
+  print (age);
+}
